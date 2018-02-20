3d2cebaede49bba59be665db2d718deeba8a53a5
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-20 21:42:47 +0100

    [ADD] HTML OK + CSS en cours

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/checkr'
-
-  get 'home/flynn'
-
-  get 'home/sparks'
+  root 'home#checkr'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
